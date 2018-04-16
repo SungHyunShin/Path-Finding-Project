298b63a6f9a3692dd6a09a92ef656fabed233110
nmarcopo <nmarcopo@nd.edu>
2018-04-16 16:38:56 -0400

    Began parsing of stdin

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,8 +1,47 @@
 // dijsktras.cpp
+#include <iostream>
+#include <map>
+using namespace std;
 
 // Main Execution
 
 int main(int argc, char *argv[]) {
+
+    // Check if next item is the end of file
+    while(cin.peek() != EOF){
+        // get number of unique tiles used
+        string TILES_N_STRING;
+        getline(cin, TILES_N_STRING);
+        int TILES_N = stoi(TILES_N_STRING);
+
+        // get weights of each tile and insert into map
+        map<char, int>  weightsMap;
+        for(int i = 0; i < TILES_N; i++){
+            char tile, weight;
+            cin >> tile;
+            cin >> weight;
+            pair<char, int> weightPair = make_pair(tile, (int)(weight - '0'));
+            weightsMap.insert(weightPair);
+        }
+
+        // get rows and cols of table
+        string rows_str, cols_str;
+        cin >> rows_str;
+        cin >> cols_str;
+        int rows, cols;
+        rows = stoi(rows_str);
+        cols = stoi(cols_str);
+
+        // make table
+        char **tileTable = new char*[rows];
+        for(int i = 0; i < rows; i++){
+            tileTable[i] = new char[cols];
+        }
+
+        // fill table
+
+    }
+
     return 0;
 }
 
